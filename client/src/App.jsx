5fcf74646bddf2ac871a97dfe2b01c22794456e3
Kyle Stubbs <kylestubbs@ucsb.edu>
2023-07-16 13:53:15 -0700

    removed unnecessary providers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,6 @@ import {
   BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
-import ColorProvider from "./context/ColorProvider";
-import GameFinishedProvider from "./context/GameFinishedProvider";
-import GameStartedProvider from "./context/GameStartedProvider";
-import GuessProvider from "./context/GuessProvider";
-import IsGuessingPlayerProvider from "./context/IsGuessingPlayerProvider";
-import SecretProvider from "./context/SecretProvider";
 import SocketProvider from "./context/SocketProvider";
 import MultiplayerGame from "./game/gamemode/MultiplayerGame";
 import SingleplayerGame from "./game/gamemode/SingleplayerGame";
@@ -20,7 +14,7 @@ import theme from "./resources/theme";
 import store from "./store/store.js";
 
 function App() {
-  function generateUrl(date) {
+  function generateUrl() {
     var text = "";
     var possible =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -33,45 +27,30 @@ function App() {
 
   return (
     <ChakraProvider theme={theme}>
-      <GameFinishedProvider>
-        <ColorProvider>
-          <GuessProvider>
-            <SecretProvider>
-              <IsGuessingPlayerProvider>
-                <GameStartedProvider>
-                  <SocketProvider>
-                    <Provider store={store}>
-                      <Router>
-                        <Routes>
-                          <Route path="/" element={<WelcomePage />} />
-                          <Route
-                            path="/singleplayer"
-                            element={<SingleplayerGame />}
-                          />
-                          <Route
-                            path="/multiplayer"
-                            on
-                            element={
-                              <Navigate
-                                to={"/multiplayer/" + generateUrl()}
-                                replace={true}
-                              />
-                            }
-                          />
-                          <Route
-                            path="/multiplayer/:gameId"
-                            element={<MultiplayerGame />}
-                          />
-                        </Routes>
-                      </Router>
-                    </Provider>
-                  </SocketProvider>
-                </GameStartedProvider>
-              </IsGuessingPlayerProvider>
-            </SecretProvider>
-          </GuessProvider>
-        </ColorProvider>
-      </GameFinishedProvider>
+      <SocketProvider>
+        <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+              <Route path="/singleplayer" element={<SingleplayerGame />} />
+              <Route
+                path="/multiplayer"
+                on
+                element={
+                  <Navigate
+                    to={"/multiplayer/" + generateUrl()}
+                    replace={true}
+                  />
+                }
+              />
+              <Route
+                path="/multiplayer/:gameId"
+                element={<MultiplayerGame />}
+              />
+            </Routes>
+          </Router>
+        </Provider>
+      </SocketProvider>
     </ChakraProvider>
   );
 }
